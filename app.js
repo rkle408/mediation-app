897acd4b7ad9fa547a8762e7b61cf937bbf2ff11
Rosebud <rkle408@gmail.com>
2022-12-20 20:35:27 -0800

    can choose beach or rain sound and video

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ const app = () => {
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
 
+    // Pick different sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function() {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        })
+    })
+
     // Play sound
     play.addEventListener('click', () => {
         checkPlaying(song);
@@ -60,6 +69,13 @@ const app = () => {
 
         // Animate the time text
         timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if(currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        } 
     };
 };
 
